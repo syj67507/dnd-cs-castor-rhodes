a6970501858d1bacabef6f0ba41fb6fd4b65c996
Shams Jamal <shams.jamal67@gmail.com>
2024-12-10 16:21:09 -0500

    Deployment: setting base to / and using actions gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { qrcode } from "vite-plugin-qrcode"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://syj67507.github.io/dnd-cs-castor-rhodes/",
+  base: "/",
   plugins: [react(), qrcode()],
   server: {
     open: true,
